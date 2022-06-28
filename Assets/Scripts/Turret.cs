243a529f68e20df36aac1ea9d0cae6dcf0d309b4
alexsmite <alexsmitem5a@gmail.com>
2022-06-27 21:29:24 -0300

    Cambios con Fabri

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -3,17 +3,48 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     public GameObject BulletPrefab;
+    public float cadence;
+    private float _rateOfFire = 0.5f;
+    private int _magazine = 0;
 
     void Start()
     {
-        ShootBullet();
+        _rateOfFire = cadence;
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+            LoadMagazine(1);
+
+        if (Input.GetKeyDown(KeyCode.J))
+            LoadMagazine(2);
+
+        if (Input.GetKeyDown(KeyCode.K))
+            LoadMagazine(3);
+
+        if (Input.GetKeyDown(KeyCode.L))
+            LoadMagazine(4);
+
+        if (_magazine > 0)
+        {
+            if (_rateOfFire <= 0)
+            {
+                Shoot();
+                _magazine -= 1;
+                _rateOfFire = cadence;
+            }
+            else
+            {
+                _rateOfFire -= Time.deltaTime;
+            }
+        }
     }
-    private void ShootBullet()
+    private void LoadMagazine(int count)
+    {
+        _magazine = count;
+    }    
+    private void Shoot()
     {
         Instantiate(BulletPrefab, transform.position, transform.rotation);
     }
